15186ae2a1e6be2d17aaf645b70ba77fc277cebc
christopher818 <christo.8.16@gmail.com>
2016-05-11 13:06:29 -0430

    cree el archivo de prueba para hacer las TDD

---
+++ BilleteraElectronica.py
@@ -0,0 +1,14 @@
+'''
+Created on 11 de may. de 2016
+
+@author: Christopher Flores 10-10824
+'''
+
+class BilleteraElectronica:
+
+    def __init__(self, identificador,nombre,apellido,ci,pin):
+     self.identificador = identificador
+     self.nombre = nombre
+     self.apellido = apellido
+     self.ci = ci
+     self.pin = pin
---
+++ test_BilleteraElectronica.py
@@ -0,0 +1,20 @@
+'''
+Created on 11 de may. de 2016
+
+@author: Christopher Flores
+'''
+import unittest
+from BilleteraElectronica import *
+from datetime import datetime 
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
