9c5fe6dc2a20dd1deb2f7fe02fec3f768a7e1d6a
isaac <isaacg311@gmail.com>
2016-05-12 11:55:21 -0400

    Segundo commit archivo principal

--- BilleteraElectronica.py
+++ BilleteraElectronica.py
@@ -2,23 +2,48 @@
 Created on 11 de may. de 2016
 
 @author: Christopher Flores 10-10824
+         Isaac Gonzalez 11-10396
+         Kervyn Rivero 11-10874
 '''
 
+#clase transaccion
+class transaccion(object):
+    
+    def __init__(self,monto, fecha, idTienda):
+        self.monto = monto
+        self.fecha = fecha
+        self.idTienda = idTienda
 
-class BilleteraElectronica:
+class BilleteraElectronica():
 
-    def __init__(self, identificador,nombre,apellido,ci,pin):
-     self.identificador = identificador
-     self.nombre = nombre
-     self.apellido = apellido
-     self.ci = ci
-     self.pin = pin
-     self.creditos = []
-     self.monto = 0
-     self.debitos = []
-     
+    def __init__(self, identificador,nombres,apellidos,ci,pin):
+
+        #Inicializamos los datos personales del dueno
+        self.identificador = identificador
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.ci = ci
+        self.pin = pin
+
+        #Inicializamos el historial financiero del dueno
+        self.creditos = []
+        self.debitos = []
+        
     def saldo(self):
-         return self.monto
+        recargas = 0
+        consumos = 0
+
+        #Cálculo de recargas
+        for i in range(len(self.creditos)):
+            recargas = recargas + self.creditos[i].monto
+
+        #Cálculo de consumos
+        for i in range(len(self.debitos)):
+            consumos = consumos + self.debitos[i].monto
+
+        saldo = recargas - consumos
+
+        return saldo
      
     def recargar(self,monto,fecha,idEstablecimiento):
         if monto <= 0:
